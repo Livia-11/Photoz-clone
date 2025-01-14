837abe0f23e8f13f7d5e415f06ac2be50d6e8939
Livia-11 <kirezilivia@gmail.com>
2025-01-14 19:54:49 +0200

    Get Mapping

--- src/main/java/com/jetbrains/photoz_clone/PhotozController.java
+++ src/main/java/com/jetbrains/photoz_clone/PhotozController.java
@@ -1,8 +1,10 @@
 package com.jetbrains.photoz_clone;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -29,6 +31,8 @@ public class PhotozController {
 
     @GetMapping("/photoz{id}")
     public Photo get(@PathVariable String id){
-        return db.get(id);
+        Photo photo = db.get(id);
+        if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
     }
 }
